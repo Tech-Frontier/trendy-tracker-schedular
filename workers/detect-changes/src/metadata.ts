a7f6874c1a0a26f980269d86076a2560a8b4e531
dp <126376945+divopsor@users.noreply.github.com>
2023-10-07 19:54:03 +0900

    feat(detect-changes): 로그 추가 및 frontend 추가 (#7)
    * feat(detect-changes): 로그 추가 및 frontend 추가
    
    * fix: 불필요한 트리거 제거

--- workers/detect-changes/src/metadata.ts
+++ workers/detect-changes/src/metadata.ts
@@ -11,5 +11,18 @@ export default [
     "itemSelector": "div > li",
     // NOTE: 해시값을 구하기 위한 dom
     "hashSelector": "#layout"
+  },
+  {
+    "id": "daangn-frontend",
+    "company": "daangn",
+    "occupation": "frontend",
+    "name": "당근 프론트엔드 개발자",
+    "url": "https://about.daangn.com/jobs/software-engineer-frontend",
+    // NOTE: 공고 목록 dom
+    "listSelector": "#layout > div > main > div:nth-of-type(3) > ul",
+    // NOTE: 공고 목록 아이템 dom
+    "itemSelector": "div > li",
+    // NOTE: 해시값을 구하기 위한 dom
+    "hashSelector": "#layout"
   }
 ];
