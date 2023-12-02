9e96c7278b9f8d65d08e088d086839af6a1b2158
divopsor <divopsor@gmail.com>
2023-12-02 20:01:07 +0900

    fix already exist

--- shared/ttapi/src/ttapi/registerRecruit.ts
+++ shared/ttapi/src/ttapi/registerRecruit.ts
@@ -7,11 +7,20 @@ interface Options {
 }
 
 export async function registerRecruit({ company, jobCategory, url }: Options) {
-  const { data } = await ttapi.post(`/api/recruit/regist`, {
-    url,
-    company,
-    jobCategory,
-  });
+  try {
 
-  return data.msg;
+    const { data } = await ttapi.post(`/api/recruit/regist`, {
+      url,
+      company,
+      jobCategory,
+    });
+
+    return data.msg;
+  } catch (error: any) {
+    if (error?.response?.data?.message === 'already exist') {
+      return '이미 존재합니다.';
+    }
+
+    throw error;
+  }
 }
